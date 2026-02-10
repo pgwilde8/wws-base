117cdd914c82fc54e0ea03444a3f2ee3c75b9210
Paul G Wilde <techsmartmarketing8@gmail.com>
2026-02-10 01:45:46 +0000

    started env

--- app/core/config.py
+++ app/core/config.py
@@ -3,7 +3,11 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
+
 class Settings:
     APP_ENV = os.getenv("APP_ENV", "template")
+    # URL path prefix for cloud storage (Spaces/S3). This app uses one folder: our-cloud-storage/dispatch
+    STORAGE_BUCKET_PREFIX = os.getenv("STORAGE_BUCKET_PREFIX", "our-cloud-storage/dispatch").rstrip("/")
+
 
 settings = Settings()
